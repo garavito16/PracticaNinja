39f99b7387de68b735cd428aeba83117e96228ff
garavito16 <lex_94@outlook.com>
2022-03-15 12:48:21 -0500

    Super Ninja sensei

--- ninja.js
+++ ninja.js
@@ -8,15 +8,56 @@ class Ninja {
         this.fuerza = 3;
     }
 
+    get getNombre(){
+        return this.nombre;
+    }
+
+    get getSalud() {
+        return this.salud;
+    }
+
+    get getVelocidad() {
+        return this.velocidad;
+    }
+
+    get getFuerza() {
+        return this.fuerza
+    }
+
+    /**
+     * @param {String} nombre
+     */
+    set setNombre(nombre){
+        this.nombre = nombre;
+    }
+
+    /**
+     * @param {number} salud
+     */
+    set setSalud(salud) {
+        this.salud = salud;
+    }
+
+    /**
+     * @param {number} velocidad
+     */
+    set setVelocidad(velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    /**
+     * @param {number} fuerza
+     */
+    set setFuerza(fuerza) {
+        this.fuerza = fuerza;
+    }
+    
     sayName() {
         console.log("El nombre del ninja es :"+this.nombre);
     }
 
     showStats () {
-        console.log("El nombre del ninja es :"+this.nombre);
-        console.log("La salud del ninja es :"+this.salud);
-        console.log("La Fuerza del ninja es :"+this.fuerza);
-        console.log("La velocidad del ninja es :"+this.velocidad);
+        console.log(`Nombre: ${this.nombre}, salud: ${this.salud}, velocidad:${this.velocidad}, fuerza:${this.fuerza}`);
     }
 
     drinkSake() {
@@ -25,7 +66,27 @@ class Ninja {
 
 }
 
-const ninja1 = new Ninja("Hyabusa");
-ninja1.sayName();
-ninja1.drinkSake();
-ninja1.showStats();
+
+class Sensei extends Ninja {
+
+    constructor(nombre) {
+        super(nombre);
+        super.setSalud = 200;
+        super.setVelocidad = 10;
+        super.setFuerza = 10;
+        this.sabiduria = 10;
+    }
+
+    speakWisdom() {
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.");
+        super.drinkSake();
+    }
+
+}
+
+// ejemplo de salida
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+superSensei.showStats();
+// -> "Nombre: Master Splinter, Salud: 210, Velocidad: 10, Fuerza: 10"
